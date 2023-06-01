5767f200f17adf723054f22f65f911ceafd4998c
easncx <cheongeason@gmail.com>
2023-06-02 00:31:52 +0800

    math lib test

--- src/SysModules/SModules.js
+++ src/SysModules/SModules.js
@@ -2,10 +2,12 @@ const JArray = require('./Array')
 const Clock = require('./Clock')
 const IO = require('./IO')
 const JMap = require('./Map')
+const JMath = require('./Math')
 
 module.exports = {
     'std:clock': new Clock(),
     'std:io': new IO(),
     'std:array': new JArray(),
-    'std:map': new JMap()
+    'std:map': new JMap(),
+    'std:math': new JMath(),
 }
